f744c597d91e410be4c1c27b33094e03a16bbad6
manubensio <pereirocondemanuel@gmail.com>
2024-09-23 12:13:33 +0200

    comentar pepito en casilla na linea 6

--- monopoly/Grupo.java
+++ monopoly/Grupo.java
@@ -19,18 +19,28 @@ class Grupo {
     * Requiere como parámetros las dos casillas miembro y el color del grupo.
      */
     public Grupo(Casilla cas1, Casilla cas2, String colorGrupo) {
+        this.colorGrupo = colorGrupo;
+        anhadirCasilla(cas2);
+        anhadirCasilla(cas1);
+
     }
 
     /*Constructor para cuando el grupo está formado por TRES CASILLAS:
     * Requiere como parámetros las tres casillas miembro y el color del grupo.
      */
     public Grupo(Casilla cas1, Casilla cas2, Casilla cas3, String colorGrupo) {
+        this.colorGrupo = colorGrupo;
+        anhadirCasilla(cas3);
+        anhadirCasilla(cas2);
+        anhadirCasilla(cas1);
     }
 
     /* Método que añade una casilla al array de casillas miembro de un grupo.
     * Parámetro: casilla que se quiere añadir.
      */
     public void anhadirCasilla(Casilla miembro) {
+        if (!this.miembros.contains(miembro))
+            this.miembros.add(miembro);
     }
 
     /*Método que comprueba si el jugador pasado tiene en su haber todas las casillas del grupo:
@@ -38,6 +48,12 @@ class Grupo {
     * Valor devuelto: true si es dueño de todas las casillas del grupo, false en otro caso.
      */
     public boolean esDuenhoGrupo(Jugador jugador) {
+        boolean esDuenho = true;
+        for (int i=0; i<this.miembros.size(); i++){
+            if (miembros.get(i).getduenhoJugador() != jugador)
+                esDuenho = false;
+        }
+        return esDuenho;
     }
 
 }
