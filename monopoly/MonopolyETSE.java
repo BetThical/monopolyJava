7024dc27aa09d3b9db230c69a5624bca7632c451
Betthical <diegongpro@gmail.com>
2024-09-30 10:45:42 +0000

    main hecho (creo xd)

--- monopoly/MonopolyETSE.java
+++ monopoly/MonopolyETSE.java
@@ -5,7 +5,6 @@ public class MonopolyETSE {
 
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(System.in);
         String comando;
 
         System.out.println("hello world\n");
@@ -14,6 +13,7 @@ public class MonopolyETSE {
         Tablero tablero = new Tablero(menu.getBanca());
 
         menu.iniciarPartida();
+        menu.loopJugable();
         
         
     }
