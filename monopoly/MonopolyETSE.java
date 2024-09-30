793be80667fbeb90c2a42db5c637343f376d6dae
Betthical <diegongpro@gmail.com>
2024-09-30 10:06:14 +0000

    iniciar partida

--- monopoly/MonopolyETSE.java
+++ monopoly/MonopolyETSE.java
@@ -5,7 +5,8 @@ public class MonopolyETSE {
 
     public static void main(String[] args) {
         System.out.println("hello world\n");
-        new Menu();
+        Menu menu = new Menu();
+        menu.iniciarPartida();
     }
     
 }
